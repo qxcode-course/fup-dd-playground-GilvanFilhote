36884e6184c731bd2709367c56c75f78cc853c25
GilvanFilhote <gilvancordeiro@alu.ufc.br>
2026-04-13 16:37:43 +0000

    ajbhgyahgv

--- fup/filhos/src/go/draft.go
+++ fup/filhos/src/go/draft.go
@@ -1,12 +1,14 @@
 package main
+
 import "fmt"
+
 func main() {
-    var filho_mais_novo, total_de_filhos, outro_filho int
-    fmt.Scan(&filho_mais_novo, &total_de_filhos)
-    for i:= 0; i<total_de_filhos; i++{
-        outro_filho+=
-        fmt.Printf("%d\n", filho_mais_novo)
-        
-    }
-    
+	var filho_mais_novo, total_de_filhos, outro_filho int
+	fmt.Scan(&filho_mais_novo, &total_de_filhos)
+	for i := 0; i < total_de_filhos; i++ {
+		outro_filho +
+			fmt.Printf("%d\n", filho_mais_novo)
+
+	}
+
 }
--- fup/final/src/go/draft.go
+++ fup/final/src/go/draft.go
@@ -1,5 +1,28 @@
 package main
 import "fmt"
 func main() {
-    fmt.Println("Hello, World!")
+    var nota1 float64
+    var nota2 float64
+    var media1 float64
+    var media2 float64
+    var notafinal float64
+
+    fmt.Scan(&nota1)
+    fmt.Scan(&nota2)
+    fmt.Scan(&notafinal)
+
+    media1= (nota1 + nota2)/2
+    media2 =(media1 + notafinal)/2
+
+    if media1 >= 7 {
+        fmt.Println("aprovado")
+    } else if media1 < 4 {
+        fmt.Println("reprovado")
+    } else if media1 >= 4 && media1 < 7 {
+     if media2 >= 5 {
+     fmt.Println("aprovado na final")
+     } else {
+        fmt.Println("reprovado na final")
+     }
+    } 
 }
