c41800fead6005694d31566909046b489ade0315
GilvanFilhote <gilvancordeiro@alu.ufc.br>
2026-04-13 16:07:26 +0000

    dkakdNJdn

---
+++ fup/filhos/src/go/draft.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+func main() {
+    var filho_mais_novo, total_de_filhos, outro_filho int
+    fmt.Scan(&filho_mais_novo, &total_de_filhos)
+    for i:= 0; i<total_de_filhos; i++{
+        outro_filho+=
+        fmt.Printf("%d\n", filho_mais_novo)
+        
+    }
+    
+}
