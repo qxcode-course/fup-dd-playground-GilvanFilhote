8217b5a5025828ea00068ffb1be1efb969200798
GilvanFilhote <gilvancordeiro@alu.ufc.br>
2026-04-13 17:47:18 +0000

    testen

---
+++ eval1/pares/src/go/draft.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+func main() {
+    var a, b, soma_par int
+    fmt.Scan(&a, &b)
+    if a<=b{
+        for i:= a; i<=b; i++{
+            if i%2==0{
+                soma_par+=i;
+            }
+        }
+        fmt.Printf("%d\n", soma_par)
+    } else {
+        fmt.Println("invalido")
+    }
+}
