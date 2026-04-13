4471116257a1d8ca7e75f6744dc47c88cd14288e
GilvanFilhote <gilvancordeiro@alu.ufc.br>
2026-04-13 17:42:48 +0000

    testeeeeen

--- eval1/monica/src/go/draft.go
+++ eval1/monica/src/go/draft.go
@@ -1,5 +1,14 @@
 package main
 import "fmt"
 func main() {
-    fmt.Println("Hello, World!")
+    var m, a, b int
+    fmt.Scan(&m, &a, &b)
+    filho:=m-(a+b)
+    if a>b && a>filho {
+        fmt.Printf("%d\n", a)
+    } else if b>a && b>filho {
+        fmt.Printf("%d\n", b)
+    } else {
+        fmt.Printf("%d\n", filho)
+}
 }
