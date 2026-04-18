b34e76fd02a7e58a199e3b9c92b04c81031529ba
GilvanFilhote <gilvancordeiro@alu.ufc.br>
2026-04-18 22:27:43 +0000

    guuuuuuggygy

--- fup/operacoes/src/go/draft.go
+++ fup/operacoes/src/go/draft.go
@@ -1,5 +1,16 @@
 package main
 import "fmt"
 func main() {
-    fmt.Println("Hello, World!")
+    var num1, num2 int
+    fmt.Scan(&num1, &num2)
+    soma:= num1 + num2
+    sub:= num1 - num2
+    mult:= num1 * num2
+    div:= float32(num1) / float32(num2)
+    resto:= float32(num1 % num2)
+    fmt.Printf("%d\n", soma)
+    fmt.Printf("%d\n", sub)
+    fmt.Printf("%d\n", mult)
+    fmt.Printf("%.2f\n", div)
+    fmt.Printf("%.0f\n", resto)
 }
