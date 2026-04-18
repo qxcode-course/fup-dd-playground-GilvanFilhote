f8e812a25b28f9a3d2323c03e1e75c8c21951805
GilvanFilhote <gilvancordeiro@alu.ufc.br>
2026-04-18 22:12:18 +0000

    jjjjjjjjjjjjj

--- fup/quebrada/go/draft.go
+++ fup/quebrada/go/draft.go
@@ -5,8 +5,9 @@ func main() {
     var a, b int
     var resto, resultado float32
     fmt.Scan(&a, &b)
-    resultado= float32 (a/b)
+    resultado = float32 (a) / float32 (b)
     resto = float32(a%b)
-    fmt.Printf("%.0f\n%.0f", resultado, resto)
-    fmt.Printf("%2.f\n", resultado)
+    fmt.Printf("%.0f\n", resultado)
+    fmt.Printf("%.0f\n", resto)
+    fmt.Printf("%.2f\n", resultado)
 }
