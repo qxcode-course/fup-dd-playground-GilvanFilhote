90830682ca981afe5810c5034e164575626d377c
GilvanFilhote <gilvancordeiro@alu.ufc.br>
2026-04-27 18:02:15 +0000

    ufyfffffffffffffffff

---
+++ fup/esconde/src/go/draft.go
@@ -0,0 +1,18 @@
+package main
+import "fmt"
+func main() {
+    var n int
+    fmt.Scan(&n)
+    for i:=1; i<=n; i++{
+        if i%2!=0{
+            fmt.Printf("%d\n", i)
+        }
+    
+    }
+    for i:=n; i==0; i--{
+        if i%2==0{
+            fmt.Printf("%d\n", i)
+        }
+    
+    }
+}
