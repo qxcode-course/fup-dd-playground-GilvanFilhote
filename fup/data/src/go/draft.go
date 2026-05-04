7f4ab239cf4cdb5d87dad32fa9c89d7bff4b1f4d
GilvanFilhote <gilvancordeiro@alu.ufc.br>
2026-05-04 19:37:27 +0000

    mmmmmmmmmma

--- fup/data/src/go/draft.go
+++ fup/data/src/go/draft.go
@@ -3,5 +3,5 @@ import "fmt"
 func main() {
     var hora, minuto, dia, mês, ano int
     fmt.Scan(&hora, &minuto, &dia, &mês, &ano)
-    fmt.Printf("0%d:%d %d/0%d/%d\n", hora, minuto, dia, mês, ano)
+    fmt.Printf("%02d:%02d %02d/%02d/%02d\n", hora, minuto, dia, mês, ano%100)
 }
