c9aaa8fb817ad8ece74f6c374bba07df003c6c4e
GilvanFilhote <gilvancordeiro@alu.ufc.br>
2026-05-04 19:47:21 +0000

    wjbwqhbeqwbrk

--- fup/opala/src/go/draft.go
+++ fup/opala/src/go/draft.go
@@ -1,5 +1,10 @@
 package main
 import "fmt"
 func main() {
-    fmt.Println("Hello, World!")
+    var velo_media, tempo_minutos, consumo_de_gaso int
+    fmt.Scan(&velo_media, &tempo_minutos, &consumo_de_gaso)
+    tempo_horas:= tempo_minutos/60
+    distancia:= tempo_horas*velo_media
+    desempenhofinal:= distancia/consumo_de_gaso
+    fmt.Printf("%.2d", desempenhofinal)
 }
