75ef397d30b35ae8eaf01c8a4335962b5b1a122e
GilvanFilhote <gilvancordeiro@alu.ufc.br>
2026-05-05 17:49:20 +0000

    pushi

--- fup/filhos/src/go/draft.go
+++ fup/filhos/src/go/draft.go
@@ -3,11 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	var filho_mais_novo, total_de_filhos, outro_filho int
+	var filho_mais_novo, total_de_filhos int
 	fmt.Scan(&filho_mais_novo, &total_de_filhos)
-	for i := filho_mais_novo; i < total_de_filhos; i++ {
-			fmt.Printf("%d\n", i)
-		outro_filho+= 1
+	for i := 0; i < total_de_filhos; i++ {
+			fmt.Printf("%d\n", filho_mais_novo)
+		filho_mais_novo+=2
 	}
 
 }
