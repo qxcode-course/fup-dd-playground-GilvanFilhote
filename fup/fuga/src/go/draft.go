f65587f2622372968e3c78c3b592462eeb5090a8
GilvanFilhote <gilvancordeiro@alu.ufc.br>
2026-05-07 02:26:25 +0000

    hbhkikb bi

--- fup/fuga/src/go/draft.go
+++ fup/fuga/src/go/draft.go
@@ -3,9 +3,29 @@ import "fmt"
 func main() {
    var h, p, f, d int
    fmt.Scan(&h, &p, &f, &d)
-   if d==-1{
-   for i:=0; i<=15; i++{
-    
-   }
-}
-}
+  if d==-1{
+    for i:=f; i<=15; i++{
+      if i>15{
+        i=0
+      } else if i==h{
+        fmt.Println("S")
+        break
+      } else if i==p{
+        fmt.Println("N")
+        break
+      }
+    }
+  } else if d==1{
+    for i:=f; i>=0; i--{
+      if i<0{
+        i=15
+      } else if i==h{
+        fmt.Println("S")
+        break
+      } else if i==p{
+        fmt.Println("N")
+        break
+      }
+    }
+  }
+  }
--- sandbox/README.md
+++ sandbox/README.md
@@ -8,4 +8,5 @@
 ## sandbox
 
 - [ ]`@user_001 `[oieeee](user_001/README.md)
+- [ ]`@user_002 `[heli](user_002/README.md)
 
