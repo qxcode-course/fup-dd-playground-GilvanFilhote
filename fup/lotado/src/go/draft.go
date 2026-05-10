1a1b291a969d3e99709c0b35a841a28f172d0346
GilvanFilhote <gilvancordeiro@alu.ufc.br>
2026-05-10 19:49:38 +0000

    jhsujwdj

--- fup/lotado/src/go/draft.go
+++ fup/lotado/src/go/draft.go
@@ -1,8 +1,21 @@
 package main
 import "fmt"
 func main() {
-    var capacidade, circulacao int
-    fmt.Scan(&capacidade, &circulacao)
-    for i=0; i<capacidade
-    fmt.Println("Hello, World!")
+    var c, m int
+    circula:=0
+    fmt.Scan(&c)
+    for i:=m; i<100 ; i++{
+        fmt.Scan(&m)
+        circula+=m
+        if circula==0{
+            fmt.Println("vazio")
+        } else if circula<0 || circula<c{
+            fmt.Println("ainda cabe")
+        } else if  circula==c || circula<2*c{
+            fmt.Println("lotado")
+        } else if circula>=2*c{
+            fmt.Println("hora de partir")
+            break
+        }
+    }
 }
