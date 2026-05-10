6465245e5e1e3aa157a30d725ad05ab7d4f4e582
GilvanFilhote <gilvancordeiro@alu.ufc.br>
2026-05-10 18:42:42 +0000

    picapi

--- fup/tv/src/go/draft.go
+++ fup/tv/src/go/draft.go
@@ -1,18 +1,16 @@
 package main
 import "fmt"
 func main() {
-    var valor, parcela_s float64
-    fmt.Scan(&valor, &parcela_s)
-    valor_total:=0
-    valor_da_parcela:=0
-    if parcela_s==1{
-        parcela_s=parcela_s-1
-        valor_da_parcela:=valor*parcela_s*0.05
-        valor_total:=valor_da_parcela*parcela_s
-        fmt.Printf("%f\n%f\n", valor_total, valor_da_parcela)
-    } else {
-        fmt.Printf("%f\n%f\n", valor_total, valor_da_parcela)
-    }
+    var valor_da_tv, parcelas float64
+    fmt.Scan(&valor_da_tv, &parcelas)
 
+    if parcelas>1{
+        juros:=(parcelas-1)*0.05
+        valor_da_tv=valor_da_tv*(1+juros)
+        parcela_da_tv:=valor_da_tv/parcelas
+        fmt.Printf("%.2f\n%.2f\n", parcela_da_tv, valor_da_tv)
+    } else {
+        fmt.Printf("%.2f\n%.2f\n", valor_da_tv, valor_da_tv)
+    }
 }
 
