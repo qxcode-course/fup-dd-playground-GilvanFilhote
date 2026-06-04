64f225c3bbb2a49558db52f8a7a57d6b4ba59874
GilvanFilhote <gilvancordeiro@alu.ufc.br>
2026-06-04 03:41:00 +0000

    bn   bn nb n

--- fup/batraquios/src/go/draft.go
+++ fup/batraquios/src/go/draft.go
@@ -5,6 +5,19 @@ func main() {
     fmt.Scan(&n, &m)
     vet_n:=make([]int, n)
     vet_m:=make([]int, m)
+    for i:=n; i<n; i++{
+        fmt.Scan(&vet_n[i])
+    }
+     for y:=m; y<m; y++{
+        fmt.Scan(&vet_m[y])
+    }
+     for i:=n; i<n; i++{
+        if vet_n[i]==vet_m[i]{
+            fmt.Println("sim")
+        } else {
+            fmt.Println("nao")
+        }
+    }
+
     
-    fmt.Prinln("nao")
 }
