b7692daf26758e9723c4e9106ea4fcb9a8a7ab1d
GilvanFilhote <gilvancordeiro@alu.ufc.br>
2026-07-01 04:08:29 +0000

    nnnnnng

--- fup/dominos/src/go/draft.go
+++ fup/dominos/src/go/draft.go
@@ -3,5 +3,19 @@ import "fmt"
 func main() {
     var n int
     fmt.Scan(&n)
-    
+    domino:= make([]int, n)
+    for i:=0; i<n; i++{
+        fmt.Scan(&domino[i])
+    }
+    ordenado:=true
+    for i:=0; i<n-1; i++{
+        if domino[i]>domino[i+1]{
+            ordenado=false
+        }
+    }
+    if ordenado==true{
+        fmt.Println("ok")
+    } else{
+        fmt.Println("precisa de ajuste")
+    }
 }
