f5125df084ad0cd589719519da153449eb0f0e69
GilvanFilhote <gilvancordeiro@alu.ufc.br>
2026-07-07 04:58:03 +0000

    hj hg h h

--- fup/pedra/src/go/draft.go
+++ fup/pedra/src/go/draft.go
@@ -1,5 +1,29 @@
 package main
 import "fmt"
 func main() {
-    fmt.Println("Hello, World!")
+    var n int
+    fmt.Scan(&n)
+    vet:= make([][2]int, n)
+
+    for i:=0; i<n; i++{
+        for j:=0; j<2; j++{
+            fmt.Scan(&vet[i][j])
+        }
+    }
+    
+    indice:=0
+    for i:=0; i<n; i++{
+        for j:=0; j<2; j++{
+            if vet[i][j]>=10{
+                if (vet[i][j]+vet[i][j+1])<(vet[i+1][j]+vet[i+1][j+1]){
+                    // vence:=(vet[i][j]+vet[i][j+1])
+                    indice=i
+                }
+            } else {
+                fmt.Println("sem ganhador")
+            }
+            
+        }
+    }
+    fmt.Print(indice)
 }
